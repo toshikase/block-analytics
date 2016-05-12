a68d1b1b5b74e78566d44fdd0fd1ad03f4c14cb1
YusukeMukoyama <yusuke.mukoyama@gmail.com>
2016-05-12 18:52:43 +0900

    change file name & edit test_mongo.rb
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	renamed:    test.rb -> test_api.rb
    	modified:   test_mongo.rb

--- test_mongo.rb
+++ test_mongo.rb
@@ -2,6 +2,6 @@
 require 'bundler/setup' #カレントディレクトリのGemを呼び出す用
 require 'mongo'
 
-connection = Mongo::Client.new([ 'localhost:27017' ], :database => 'test')
+connection = Mongo::Client.new([ 'localhost:27017' ])
 puts 'データベース一覧'
 puts connection.database_names
